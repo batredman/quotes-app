7602509f73b62a55b3f3e54325de248e8bdad9f5
AH <batredman123@gmail.com>
2018-06-13 23:21:25 -0600

    First

---
+++ src/app/app.module.ts
@@ -0,0 +1,47 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { FavoritesPage } from '../pages/favorites/favorites';
+import { QuotePage } from '../pages/quote/quote';
+import { QuotesPage } from '../pages/quotes/quotes';
+import { SettingsPage } from '../pages/settings/settings';
+import { LibraryPage } from '../pages/library/library';
+import { TabsPage } from '../pages/tabs/tabs';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    FavoritesPage,
+    QuotePage,
+    QuotesPage,
+    SettingsPage,
+    LibraryPage,
+    TabsPage
+
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    FavoritesPage,
+    QuotePage,
+    QuotesPage,
+    SettingsPage,
+    LibraryPage,
+    TabsPage
+
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
